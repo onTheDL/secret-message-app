2c34dbbc3e03bbadccf54c776ac0525a05dc89d7
Daniel Laserna <daniel.laserna@protonmail.com>
2021-03-05 21:22:26 -0500

    Embed encoded message in url, then extract into console

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+// Extracting message from url
+const { hash } = window.location
+// Remove extra hash from url
+console.log(atob(hash.replace('#', '')))
+
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault(); // prevents the browser from automatically sending form input to backend server
+
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
+
   const input = document.querySelector("#message-input");
   // Using Base64 for encryption
-  const encrypted = btoa(input.value)
+  const encrypted = btoa(input.value);
 
-  document.querySelector("#link-input").value = encrypted
+  const input2 = document.querySelector("#link-input");
+
+  input2.value = `${window.location}#${encrypted}`;
+  input2.select();
 });
